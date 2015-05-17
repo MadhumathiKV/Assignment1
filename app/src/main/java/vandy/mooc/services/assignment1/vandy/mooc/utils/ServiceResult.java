43d59c713d0861e188676797d572a008bbc11d84
Smartron01 <smartron01@Smartron01s-MacBook-Pro.local>
2015-05-17 12:47:13 +0530

    Initial commit

---
+++ app/src/main/java/vandy/mooc/services/assignment1/vandy/mooc/utils/ServiceResult.java
@@ -0,0 +1,23 @@
+package vandy.mooc.services.assignment1.vandy.mooc.utils;
+
+/**
+ * Created by smartron01 on 17/05/15.
+ */
+import android.os.Bundle;
+
+/**
+ * Interface that's implemented by an Activity that wants to receive
+ * the results of a Service.
+ */
+public interface ServiceResult {
+    /**
+     * Called when a launched Service sends back results from
+     * computations it runs, giving the requestCode it was started
+     * with, the resultCode it returned, and any additional data from
+     * it.  The resultCode will be RESULT_CANCELED if the Service
+     * explicitly returned that.
+     */
+    void onServiceResult(int requestCode,
+                         int resultCode,
+                         Bundle data);
+}
